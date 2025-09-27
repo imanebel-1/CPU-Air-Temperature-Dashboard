e51b262e243a575e5f4b583d9b43247914c65e88
imanebel-1 <imanebel.87@gmail.com>
2025-09-27 14:06:29 +0100

    Converted timestamps to readable format.

--- programming_assignment.py
+++ programming_assignment.py
@@ -1,10 +1,20 @@
 import csv
-#load data into CSV file:
+from datetime import datetime
+# Load data from CSV
 data = []
 with open("tempLog-1.csv", "r") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        time_stamp = float(row['Timestamp'])
+        # Convert numeric timestamp to datetime
+        time_stamp_num = float(row['Timestamp'])
+        time_stamp = datetime.fromtimestamp(time_stamp_num)  # human-readable datetime
+        
         air_temp = float(row['Air'])   # air temperature
-        cpu_temp = float(row['CPU'])  # cpu temperature
+        cpu_temp = float(row['CPU'])   # cpu temperature
+        
         data.append({'Timestamp': time_stamp, 'Air': air_temp, 'CPU': cpu_temp})
+
+        
+# print first 5 rows to see results
+for d in data[:5]:
+    print(d)
